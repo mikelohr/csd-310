846bd7a5a2bbcf9ca4a339f12d0bb51590190ed4
Mike Lohr <mikelohr70@gmail.com>
2021-06-30 16:59:21 -0500

    Upload successful

---
+++ module_8/pysports_queries.py
@@ -0,0 +1,26 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+    "user": "pysports_user",
+    "password": "Colin",
+    "host": "127.0.0.1",
+    "database": "pysports",
+    "raise_on_warnings": True
+}
+
+try: 
+    db = mysql.connector.connect(**config)
+    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+    input("\n\n Press any key to continue...")
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACESS_DENIED ERROR:
+        print(" The supplied user name or password are invalid")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print(" The specified database does not exist")
+    else:
+        print(err)
+finally:
+    db.close()
+
